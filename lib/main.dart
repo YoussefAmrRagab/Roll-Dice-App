66b0cca1e4aba96224586e5b54bdfe9dd3d7db27
Youssef Amr <youssefamr2244@gmail.com>
2023-05-23 01:05:03 +0300

    background color changed

--- lib/main.dart
+++ lib/main.dart
@@ -22,24 +22,35 @@ class _RollDiceState extends State<RollDice> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 47, 14, 101),
-      body: Center(
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Image.asset(
-            "assets/images/dice-$num.png",
-            height: 150,
-            width: 150,
-          ),
-          const SizedBox(height: 10),
-          TextButton(
-              onPressed: () => setState(() {
-                    num = randomizer.nextInt(6) + 1;
-                  }),
-              child: const Text(
-                "Roll Dice",
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ))
-        ]),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 168)
+            ])),
+        child: Center(
+          child: Column(mainAxisSize: MainAxisSize.min, children: [
+            Image.asset(
+              "assets/images/dice-$num.png",
+              height: 150,
+              width: 150,
+            ),
+            const SizedBox(height: 10),
+            TextButton(
+                onPressed: () => setState(() {
+                      num = randomizer.nextInt(6) + 1;
+                    }),
+                child: const Text(
+                  "Roll Dice",
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                ))
+          ]),
+        ),
       ),
     );
   }
